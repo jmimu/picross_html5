a63f3aae215919de6ccaa0836107caaeadb9463b
jmimu <jmimu@free.fr>
2016-01-02 20:56:21 +0100

    text + scale

--- scripts/main.js
+++ scripts/main.js
@@ -13,31 +13,52 @@ $(document).ready(function() //or $(function()
 
 
 function drawGrid(svg) {
-  draw_size_ppx = 300; //ppx = pseudo pixel
-  info_size_ppx = 200;
+  
+  
   grid_cols = 10;
   
-  col_w_ppx = draw_size_ppx/grid_cols;
+  col_w_ppx = 25;//ppx = pseudo pixel
+  info_size_ppx = 15*grid_cols;
+  draw_size_ppx = info_size_ppx+col_w_ppx*grid_cols;
   
   
-  var g = svg.group({stroke: 'black', strokeWidth: 2});
+  window_size=$('#svgbasics').innerWidth();
+  if ($('#svgbasics').innerHeight()<window_size)
+    window_size=$('#svgbasics').innerHeight();
+  scale_factor=window_size/(draw_size_ppx)*.95;
+  
+  
+  var g = svg.group({stroke: 'black', strokeWidth: 2, transform: 'scale('+scale_factor+')'});
+  
+  
   for (i = 0; i <= grid_cols; i++) {
     y=info_size_ppx+col_w_ppx*i;
-    svg.line(g,0, y, draw_size_ppx+info_size_ppx, y);
+    svg.line(g,0, y, draw_size_ppx, y);
   }
   
   for (i = 0; i <= grid_cols; i++) {
     x=info_size_ppx+col_w_ppx*i;
-    svg.line(g,x,0, x, draw_size_ppx+info_size_ppx);
+    svg.line(g,x,0, x, draw_size_ppx);
   }
   
   
   text_w_pps = 30;
   
   var all_texts=["2","4","12","5"];
+  //test: hz
   for (i=0;i<all_texts.length;i++)
   {
-    svg.text(g,info_size_ppx-(all_texts.length-i)*text_w_pps,info_size_ppx+col_w_ppx,all_texts[i]);
+    decal=0;
+    if (all_texts[i].length>1) decal=-5;
+    svg.text(g,info_size_ppx-(all_texts.length-i-0.5)*text_w_pps+decal,info_size_ppx+col_w_ppx-6,all_texts[i]);
+  }
+  
+  //test: vert
+  for (i=0;i<all_texts.length;i++)
+  {
+    decal=0;
+    if (all_texts[i].length>1) decal=-5;
+    svg.text(g,info_size_ppx+7+decal,info_size_ppx-(all_texts.length-i-1)*text_w_pps-6,all_texts[i]);
   }
   
 }
