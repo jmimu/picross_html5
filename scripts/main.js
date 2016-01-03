59e5b7fbfbf69ee54b15d8f5bb9f1dbb832a1590
jmimu <jmimu@free.fr>
2016-01-03 18:04:03 +0100

    compute info

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,105 @@
+var main_svg=0;
+
 $(document).ready(function() //or $(function()
   {
     $('#svgbasics')[0].oncontextmenu = function() {return false;} //remove left click menu on svg part
     
     $('#svgbasics').svg({onLoad: drawGrid});
     
-    //$('#svg_grid').empty();
-    //var svg = $('#svgbasics').svg('get');
-    //$('#svgbasics').svg('get').circle(130, 75, 50, {fill: 'none', stroke: 'red', strokeWidth: 3});
-    /*for (i = 0; i < 11; i++) {
-      svg.circle(130, 75, 50, {fill: 'none', stroke: 'red', strokeWidth: 3});
-    }*/
   }
 );
 
 function compute_hz_numbers(grid)
 {
   hz_numbers=[];
-  hz_numbers.push([1,1,1]);
+  
+  for (j=0;j<grid.length;j++)
+  {
+    line_numbers=[];
+    accumulator=0;
+    for (i=0;i<grid[j].length;i++)
+    {
+      if (grid[j][i]==1) 
+      {
+        accumulator+=1;
+      }
+      if ((grid[j][i]==0)&&(accumulator>0))
+      {
+        line_numbers.push(accumulator);
+        accumulator=0;
+      }
+    }
+    if (accumulator>0) line_numbers.push(accumulator);
+    if (line_numbers.length==0) line_numbers.push(0);
+    
+    hz_numbers.push(line_numbers);
+  }
   return hz_numbers;
 }
 
-function squareClick(evt) { 
-    console.log("click");
-    var square = evt.target;
-    var square_status=square.getAttribute("status"); 
-    if (square_status==0)
+function compute_vert_numbers(grid)
+{
+  hz_numbers=[];
+  for (i=0;i<grid.length;i++)
+  {
+    line_numbers=[];
+    accumulator=0;
+    for (j=0;j<grid[i].length;j++)
     {
-      square.setAttribute("status", 1); 
-      square.setAttribute("fill", 'purple');
-    }else{
-      square.setAttribute("status", 0); 
-      square.setAttribute("fill", 'white');
+      if (grid[j][i]==1) 
+      {
+        accumulator+=1;
+      }
+      if ((grid[j][i]==0)&&(accumulator>0))
+      {
+        line_numbers.push(accumulator);
+        accumulator=0;
+      }
     }
+    if (accumulator>0) line_numbers.push(accumulator);
+    if (line_numbers.length==0) line_numbers.push(0);
     
-  } 
+    hz_numbers.push(line_numbers);
+  }
+  return hz_numbers;
+}
+
+function squareLeftClick(evt) { 
+  console.log("Lclick");
+  var square = evt.target;
+  var square_status=square.getAttribute("status"); 
+  if (square_status!=1)
+  {
+    square.setAttribute("status", 1); 
+    square.setAttribute("fill", 'black');
+  }else{
+    square.setAttribute("status", 0); 
+    square.setAttribute("fill", 'white');
+  }
+}
+
+function squareRightClick(evt) { 
+  console.log("Rclick");
+  var square = evt.target;
+  var square_status=square.getAttribute("status"); 
+  if (square_status!=2)
+  {
+    square.setAttribute("status", 2); 
+    square.setAttribute("fill", 'gray');
+    ///TODO: draw an x ?
+  }else{
+    square.setAttribute("status", 0); 
+    square.setAttribute("fill", 'white');
+  }
+}
 
 function drawGrid(svg) {
-  grid_solution = [[0,0,0,0,0],
+  main_svg=svg;
+  grid_solution = [[0,1,0,1,0],
                    [1,1,1,1,1],
-                   [0,1,0,1,0],
                    [0,0,1,0,0],
-                   [1,1,0,1,1]];
+                   [1,1,0,1,1],
+                   [0,0,0,0,0]];
   
   grid_cols = 5;
   
@@ -72,14 +131,14 @@ function drawGrid(svg) {
     y=info_size_ppx+col_w_ppx*j;
     for (i = 0; i < grid_cols; i++) {
       x=info_size_ppx+col_w_ppx*i;
-      var rect=svg.rect(g, x, y, col_w_ppx, col_w_ppx, {onclick: 'squareClick(evt)', fill: 'white', stroke: 'purple', strokeWidth: 1});
+      var rect=svg.rect(g, x, y, col_w_ppx, col_w_ppx, {onclick: 'squareLeftClick(evt)', fill: 'white', stroke: 'black', strokeWidth: 1});
       rect.setAttribute("status", 0);
+      rect.addEventListener('contextmenu', squareRightClick, false);
     }
   }
   
   text_w_pps = 30;
   
-  //var all_hz_texts=[["2","4","12","5"],["2","1","12","5"],["2","12","9"]];
   all_hz_texts=compute_hz_numbers(grid_solution);
   
   //test: hz
@@ -93,7 +152,7 @@ function drawGrid(svg) {
     }
   }
   
-  var all_vert_texts=[["2","4","12","5"],["2","8","12","5"],["4","12","5"]];
+  var all_vert_texts=compute_vert_numbers(grid_solution);
   //test: vert
   for (j=0;j<all_vert_texts.length;j++)
   {
