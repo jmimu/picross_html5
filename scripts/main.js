95687d15dffae4d2117fbb57d90fd54f28ad3d63
jmimu <jmimu@free.fr>
2016-01-03 15:19:48 +0100

    begin click

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() //or $(function()
   {
+    $('#svgbasics')[0].oncontextmenu = function() {return false;} //remove left click menu on svg part
+    
     $('#svgbasics').svg({onLoad: drawGrid});
     
     //$('#svg_grid').empty();
@@ -11,14 +13,39 @@ $(document).ready(function() //or $(function()
   }
 );
 
+function compute_hz_numbers(grid)
+{
+  hz_numbers=[];
+  hz_numbers.push([1,1,1]);
+  return hz_numbers;
+}
+
+function squareClick(evt) { 
+    console.log("click");
+    var square = evt.target;
+    var square_status=square.getAttribute("status"); 
+    if (square_status==0)
+    {
+      square.setAttribute("status", 1); 
+      square.setAttribute("fill", 'purple');
+    }else{
+      square.setAttribute("status", 0); 
+      square.setAttribute("fill", 'white');
+    }
+    
+  } 
 
 function drawGrid(svg) {
+  grid_solution = [[0,0,0,0,0],
+                   [1,1,1,1,1],
+                   [0,1,0,1,0],
+                   [0,0,1,0,0],
+                   [1,1,0,1,1]];
   
-  
-  grid_cols = 10;
+  grid_cols = 5;
   
   col_w_ppx = 25;//ppx = pseudo pixel
-  info_size_ppx = 15*grid_cols;
+  info_size_ppx = 18*grid_cols;
   draw_size_ppx = info_size_ppx+col_w_ppx*grid_cols;
   
   
@@ -41,24 +68,51 @@ function drawGrid(svg) {
     svg.line(g,x,0, x, draw_size_ppx);
   }
   
+  for (j = 0; j < grid_cols; j++) {
+    y=info_size_ppx+col_w_ppx*j;
+    for (i = 0; i < grid_cols; i++) {
+      x=info_size_ppx+col_w_ppx*i;
+      var rect=svg.rect(g, x, y, col_w_ppx, col_w_ppx, {onclick: 'squareClick(evt)', fill: 'white', stroke: 'purple', strokeWidth: 1});
+      rect.setAttribute("status", 0);
+    }
+  }
   
   text_w_pps = 30;
   
-  var all_texts=["2","4","12","5"];
+  //var all_hz_texts=[["2","4","12","5"],["2","1","12","5"],["2","12","9"]];
+  all_hz_texts=compute_hz_numbers(grid_solution);
+  
   //test: hz
-  for (i=0;i<all_texts.length;i++)
+  for (j=0;j<all_hz_texts.length;j++)
   {
-    decal=0;
-    if (all_texts[i].length>1) decal=-5;
-    svg.text(g,info_size_ppx-(all_texts.length-i-0.5)*text_w_pps+decal,info_size_ppx+col_w_ppx-6,all_texts[i]);
+    for (i=0;i<all_hz_texts[j].length;i++)
+    {
+      decal=0;
+      if (all_hz_texts[j][i].length>1) decal=-5;
+      svg.text(g,info_size_ppx-(all_hz_texts[j].length-i-0.5)*text_w_pps+decal,info_size_ppx+col_w_ppx*j+col_w_ppx-6,all_hz_texts[j][i].toString());
+    }
   }
   
+  var all_vert_texts=[["2","4","12","5"],["2","8","12","5"],["4","12","5"]];
   //test: vert
-  for (i=0;i<all_texts.length;i++)
+  for (j=0;j<all_vert_texts.length;j++)
   {
-    decal=0;
-    if (all_texts[i].length>1) decal=-5;
-    svg.text(g,info_size_ppx+7+decal,info_size_ppx-(all_texts.length-i-1)*text_w_pps-6,all_texts[i]);
+    for (i=0;i<all_vert_texts[j].length;i++)
+    {
+      decal=0;
+      if (all_vert_texts[j][i].length>1) decal=-5;
+      svg.text(g,info_size_ppx+7+decal+col_w_ppx*j,info_size_ppx-(all_vert_texts[j].length-i-1)*text_w_pps-6,all_vert_texts[j][i].toString());
+    }
   }
   
 }
+
+
+
+
+
+
+
+
+
+
